b497fdbe821bb4205c4bf22b323fe601c5b1a4dc
nagarajuchikulla <chikullanagaraju7@gmail.com>
2022-10-15 15:26:06 +0530

    basisc function

--- functions/function.js
+++ functions/function.js
@@ -1,45 +1,144 @@
-function name(val1,val2){
-    var result=val1+val2
+function name(val1, val2) {
+    var result = val1 + val2
     console.log(result);
 }
-name(5,6)
+name(5, 6)
 
 
 
 
 
 
-function manu(num1,num2){
-    var ram=num1*num2
-    console.log(ram);
-    return ram;
+// function manu(num1,num2){
+//     var ram=num1*num2
+//     console.log(ram);
+//     return ram;
+// }
+// console.log(manu(7,7));
+// console.log(manu(7,7));
+// console.log(manu(7,7));
+// console.log(manu(7,7));
+
+
+
+function hug(num1, num2) {
+    var boy = num1 - num2
+    let nani = num1 + num2
+    const anil = num1 * num2
+    console.log(boy, nani, anil);
+    return boy, nani, anil;
 }
-console.log(manu(7,7));
-console.log(manu(7,7));
-console.log(manu(7,7));
-console.log(manu(7,7));
+console.log(hug(8, 5))
 
 
 
-function hug(num1,num2){
-    var boy=num1-num2
-    let nani=num1+num2
-    const anil=num1*num2
-    console.log(boy,nani,anil);
-    return boy,nani,anil;
+
+
+
+// function jock(){
+// let kill="nagaraju"
+// var gun=23+45
+// const bill=6*5
+// console.log(kill,gun,bill);
+// return bill;
+// }
+// console.log(jock());
+
+
+// undefined,false,null , "", 0 ==>false
+
+// "qwer",1234,true,[],{} ==>true
+
+if (!"") {
+    console.log("manu");
+} else if (!true) {
+    console.log("nagaraju");
+} else {
+    console.log('default');
 }
-console.log(hug(8,5))
 
 
-
-
-
-
-function jock(){
-let kill="nagaraju"
-var gun=23+45
-const bill=6*5
-console.log(kill,gun,bill);
-return bill;
+function nag(num) {
+    return num * 4
 }
-console.log(jock());
+console.log(nag(5));
+
+function kill(num) {
+    return num + 6;
+}
+console.log(kill(8));
+
+
+var sum = 0
+function name() {
+    sum = sum * 4;
+}
+
+
+
+function mineSeven(num) {
+    return num - 6;
+}
+console.log(mineSeven(8));
+
+
+
+function ourName() {
+    console.log("hellow world");
+}
+ourName();
+
+
+
+function reuseble() {
+    console.log("money");
+}
+reuseble();
+
+
+function nag(a, b) {
+    console.log(a + b);
+}
+nag(10, 5)
+
+
+function sri(m, n) {
+    console.log(m * n);
+}
+sri(25, 5);
+
+
+let raju = "nag";
+function myName() {
+    let raju = "dfghj";
+    return raju;
+}
+console.log(myName);
+console.log(raju);
+
+
+function hill(sum) {
+    console.log(sum + 6);
+
+}
+hill(5);
+
+
+
+function mom(val1, val2) {
+    var dog = val1 + val2;
+    let sess = val1 * val2;
+    console.log(dog, sess);
+}
+mom(8, 9);
+
+
+function rock(num1,num2){
+    let jill= num1/num2
+    let nut=num1*num2
+    return nut;
+}
+console.log("nut",rock(6,7));
+
+
+
